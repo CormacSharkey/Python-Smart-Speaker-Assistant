67a4dbb5122f18dd10a31c2af38ab3159567c5fa
CormacSharkey <csharkey@tcd.ie>
2024-08-31 22:56:01 +0100

    adding audio file save functionality

--- speaker.py
+++ speaker.py
@@ -2,3 +2,8 @@ import pyttsx3
 
 def speak_text(text: str): 
     pyttsx3.speak(text)
+
+def save_text(text: str, filename: str):
+    engine = pyttsx3.init()
+    engine.save_to_file(text, filename)
+    engine.runAndWait()
