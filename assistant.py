360c6f848e51f64f94091bedf3ca58214e69fa7c
CormacSharkey <csharkey@tcd.ie>
2024-09-05 23:21:07 +0100

    updating TODO list and prepping for YT functionality

--- assistant.py
+++ assistant.py
@@ -1,6 +1,7 @@
 import listener as ls
 import speaker as sp
 import datetime as dt
+from youtube_search import YoutubeSearch
 import human_readable as hr
 
 nametag = "Sherlock"
@@ -13,5 +14,21 @@ def eventLoop():
             if "what time is it" in speech:
                 current_time = dt.datetime.now()
                 sp.speak_text("It is " + hr.timing(current_time, formal=False) + ".")
+            elif "Youtube".casefold() in speech:
+                query = speech.replace(r"Play ".casefold(), '').replace(r" on Youtube".casefold(), '')
+                result = youtube_search(query)
 
-    
+
+
+
+
+
+
+def youtube_search(query):
+    result = YoutubeSearch(query, max_results=1).to_dict()
+    result_url = result.get("title"), "https://www.youtube.com" + result.get("url_suffix")
+
+    return result_url
+
+def youtube_play(query):
+    print("Hello")
--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import radioactive.alias
 import assistant
 import human_readable as hr
 import datetime as dt
+import time
+
+# TODO: Play and search radio stations
+# TODO: Send and receive WhatsApp messages
+# TODO: Access ChatGPT
+# TODO: Get the current time
+# TODO: Add reminders and timers
+# TODO: Play and search radio 
+# TODO: Play Youtube Audio
+
 
 
 
@@ -16,4 +27,7 @@ if __name__ == "__main__":
     # print(hr.timing(dt.time(0, 20, 40), formal=False))
     # print(hr.timing(dt.time(6, 25, 40), formal=False))
 
-    assistant.sp.speak_text("We out here walking, like back in the old days.")
+    import radioactive
+
+
+    # assistant.sp.speak_text("We out here walking, like back in the old days.")
